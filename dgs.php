bc2d8364c7a6dcb8576f5166583a6e9f872243c4
Henri Kauppinen <henri.kauppinen@orum.fi>
2013-01-30 01:29:40 +0200

    index.html / dgs.php

---
+++ dgs.php
@@ -0,0 +1,33 @@
+<?php
+
+header('Content-type:text/html;Charset=utf-8');
+
+require_once 'models/db_model.php';
+require_once 'controllers/controller.php';
+require_once 'helper.php';
+
+session_start();
+
+if(!(isset($_SESSION['logged'])))
+{
+	require_once 'models/login_model.php';
+	require_once 'controllers/login_controller.php';
+	$dgs = new login_controller();
+	
+}
+else {
+
+	$controller = param('p');
+
+	if($controller == '') {
+		$controller = 'dgs';
+	}
+
+	require_once 'models/dgs_model.php';
+
+	require_once "controllers/{$controller}_controller.php";
+	
+	$classname = $controller.'_controller';
+	$app = new $classname();
+
+}
